1d14ac2dcb2cbf11cbe34557ca8bb9338a14a390
Paulhakeem <paulnyamawi18@gmail.com>
2023-11-04 15:18:31 +0300

    chat app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="m-auto justify-center h-auto text-center mt-20 max-w-md border-4 border-primary rounded-md select-none">
+    <h1 class="text-secondary font-medium text-2xl mt-8">P.R.E Chat App</h1>
+   <form class="text-center my-8">
+     <h2 class="text-primary text-xl">Enter your user name</h2>
+     <div class="mx-4 my-2">
+      <input type="text" class="outline-none focus:ring-0 w-full bg-[#000] border border-1 border-primary rounded-md text-primary p-2">
+     </div>
+     <div class="mx-4 my-4">
+      <button class="uppercase text-xl bg-secondary text-primary w-full p-2 font-semibold">save</button>
+     </div>
+     
+   </form>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': '#dcdcdc',
+      'secondary': '#4cc2ff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
